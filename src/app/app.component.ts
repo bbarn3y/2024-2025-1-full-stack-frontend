8bec5c9dc842ebc0262ae4f586892b6fe88b9214
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-09-17 08:37:54 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = '2024-2025-full-stack-frontend';
+}
